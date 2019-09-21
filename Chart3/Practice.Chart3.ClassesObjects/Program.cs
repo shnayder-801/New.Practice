6d79e3cc892335dc942d2789df3b3730999ed2b1
Leonid <shnayder.801@mail.ru>
2019-09-21 13:27:52 +0300

    Создал два проекта

---
+++ Chart3/Practice.Chart3.ClassesObjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Chart3.ClassesObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
