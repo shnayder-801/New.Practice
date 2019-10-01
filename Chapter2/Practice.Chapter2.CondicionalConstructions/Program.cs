412dbfde6f4acc1d2e56812911fd5b1622bdfb3b
Leonid <shnayder.801@mail.ru>
2019-10-01 17:30:07 +0300

    Создал проект с конструкцией switch...case

--- Chapter2/Practice.Chapter2.CondicionalConstructions/Program.cs
+++ Chapter2/Practice.Chapter2.CondicionalConstructions/Program.cs
@@ -6,38 +6,18 @@ namespace Practice.Chapter2.CondicionalConstructions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Нажмите Y или N");
+            Console.WriteLine("Нажмите любую клавишу");
             var selection = Console.ReadLine();
             switch (selection)
             {
-                case "Y":
-                    Console.WriteLine("Вы нажали Y");
-                    break;
-                case "N":
-                    Console.WriteLine("Вы нажали N");
+                case "a":
+                    Console.WriteLine($"Вы нажали {selection}");
                     break;
                 default:
-                    Console.WriteLine("Вы нажали неизвестную букву");
+                    Console.WriteLine($"Вы нажали {selection}");
                     break;
             }
             Console.ReadKey();
-            /*
-            var a = 12;
-            var b = 12;
-            if (a < b && a == 9)
-            {
-                Console.WriteLine($"Число {a} меньше числа {b}");
-                }
-                else if (a > b)
-                {
-                    Console.WriteLine($"Число {a} больше числа {b}");
-                }
-                else
-                {
-                    Console.WriteLine($"Число {a} равно числу {b}");
-                }
-                Console.ReadLine();
-                */
-        }
+            }
     }
 }
