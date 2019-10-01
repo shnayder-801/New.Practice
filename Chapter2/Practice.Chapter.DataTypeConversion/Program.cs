8248ea6ad3d1811fe70c0d1f96d7306df91ebcd3
Leonid <shnayder.801@mail.ru>
2019-10-01 13:47:42 +0300

    Создал проект с преобразованием базовых типов данных
    преобразование int в byte, использование значения checked

---
+++ Chapter2/Practice.Chapter.DataTypeConversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practice.Chapter.DataTypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 12;
+                byte c = checked((byte) (a + b));
+                Console.WriteLine($"{c}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                /*
+                byte a = 20;
+                byte b = (byte) (a + 70);
+                Console.WriteLine($"{b}");
+                Console.ReadLine();
+                */
+
+            }
+        }
+    }
+}
