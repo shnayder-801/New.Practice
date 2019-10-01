5f1240af784494632d71859fc7b41187d907e76f
Leonid <shnayder.801@mail.ru>
2019-10-01 22:25:10 +0300

    Cоздал проект с массивами

---
+++ Chapter2/Practice.Chapter2.Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice.Chapter2.Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] a= new int[10];
+            for (int i = 0; i < a.Length; i++)
+            {
+             Console.WriteLine(i);
+                Console.ReadKey();
+            }
+
+            /*
+            int [] i = new int [4];
+            foreach (var a in i)
+            {
+                Console.WriteLine(i);
+                Console.ReadKey();
+                */
+            
+            /*
+            int [] i = new int [4];
+            i [0] = 1;
+            i [1] = 2;
+            i [2] = 3;
+            i [3] = 4;
+            Console.WriteLine(i[2]);
+            Console.ReadKey();
+            */
+        }
+    }
+}
