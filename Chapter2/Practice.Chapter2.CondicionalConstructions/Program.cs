6d1f3e755f962224da79a6e5bed93d5a56021ec6
Leonid <shnayder.801@mail.ru>
2019-10-01 17:18:41 +0300

    Создал проект с условными конструкциями
    конструкции if...else, switch...case

---
+++ Chapter2/Practice.Chapter2.CondicionalConstructions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice.Chapter2.CondicionalConstructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите Y или N");
+            var selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "Y":
+                    Console.WriteLine("Вы нажали Y");
+                    break;
+                case "N":
+                    Console.WriteLine("Вы нажали N");
+                    break;
+                default:
+                    Console.WriteLine("Вы нажали неизвестную букву");
+                    break;
+            }
+            Console.ReadKey();
+            /*
+            var a = 12;
+            var b = 12;
+            if (a < b && a == 9)
+            {
+                Console.WriteLine($"Число {a} меньше числа {b}");
+                }
+                else if (a > b)
+                {
+                    Console.WriteLine($"Число {a} больше числа {b}");
+                }
+                else
+                {
+                    Console.WriteLine($"Число {a} равно числу {b}");
+                }
+                Console.ReadLine();
+                */
+        }
+    }
+}
