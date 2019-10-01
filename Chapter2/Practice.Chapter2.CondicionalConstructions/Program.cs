56f5b468140eab1882364fcd15920eabb8b59d94
Leonid <shnayder.801@mail.ru>
2019-10-01 18:59:11 +0300

    Создал проект с тернарной операцией

--- Chapter2/Practice.Chapter2.CondicionalConstructions/Program.cs
+++ Chapter2/Practice.Chapter2.CondicionalConstructions/Program.cs
@@ -6,6 +6,32 @@ namespace Practice.Chapter2.CondicionalConstructions
     {
         static void Main(string[] args)
         {
+            int y = 6;
+            int x = 5;
+            Console.WriteLine("Нажмите + или -");
+            string selected = Console.ReadLine();
+
+            int z = selected == "+"?(x + y):(x - y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+            
+            /*
+            int i = 30;
+            switch (i)
+            {
+                case 10:
+                    Console.WriteLine("case 10");
+                    goto case 20;
+                case 20:
+                    Console.WriteLine("case 20");
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+                        Console.ReadKey();
+                        */
+            /*
             Console.WriteLine("Нажмите любую клавишу");
             var selection = Console.ReadLine();
             switch (selection)
@@ -18,6 +44,7 @@ namespace Practice.Chapter2.CondicionalConstructions
                     break;
             }
             Console.ReadKey();
-            }
+            */
+        }
     }
 }
