a608498b754a2e470675dd678484855cdf9cc7bd
Leonid <shnayder.801@mail.ru>
2019-10-03 17:12:27 +0300

    Создал проект с процессом передачи значений переменных
    входные, выходные, по значению, по ссылке

---
+++ Chapter2/Practice.Chapter2.Options/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Practice.Chapter2.Options
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int area;
+            int perim;
+            GetDate(a, 15, out area, out perim);
+            Console.WriteLine("Площадь: " +area);
+            Console.WriteLine("Периметр: " +perim);
+            Console.ReadKey();
+        }
+
+        static void GetDate(int x, int y, out int area, out int perim)
+        {
+            area = x * y;
+            perim = (x + y) * 2;
+        }
+    }
+}
+
+
+        /*static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine($"Начальное значение переменной = {a}");
+            Incremental(ref a);
+            Console.WriteLine($"Переменная а после передачи по значению = {a}");
+            Console.ReadLine();
+        }
+
+        private static void Incremental(ref int x)
+        {
+            x++;
+            Console.WriteLine($"Incremental: {x}");
+        }
+    }
+}*/
+
+
