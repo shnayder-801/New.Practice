0d06c54c54f97aa78ad3627a296581ff3637feb6
Leonid <shnayder.801@mail.ru>
2019-10-07 21:17:03 +0300

    Создал проект работы с кортежами
    функция не работае!!! (Разобраться)

---
+++ Chapter2/Practice.Chapter2.Cortege/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Practice.Chapter2.Cortege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = GetValue(new int[] {1, 2, 3, 4, 5});
+            Console.WriteLine(tuple.sum);
+            Console.WriteLine(tuple.count);
+            Console.Read();
+        }                                                                  /* не работает ?????? Предположительно в версии отсутствует 
+                                                                              функция работы с кортежами */
+
+        static (int sum, int count) GetValue(int[] numbers)
+        {
+            var result = (sum: 0, count: 0);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result.sum += numbers[i];
+                result.count++;
+            }
+
+            return result;
+        }
+    }
+}
+
+
+           /* var tuple =Hay(45, 4);
+            Console.WriteLine(tuple);
+            Console.WriteLine("{0}");
+
+            Console.Read();
+        }
+        static int Hay(int x, int y)
+        { 
+            var result = x*y;
+            return result;
+        }
+    }
+}*/
