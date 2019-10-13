065c7b8e83ae6a568a1abde86e779c3147b08db2
Leonid <shnayder.801@mail.ru>
2019-10-13 15:55:12 +0300

    Создал проект без примера в рамках работы с классами

--- Chapter3/Practice.Chapter3.ClassesAndObjects/Program.cs
+++ Chapter3/Practice.Chapter3.ClassesAndObjects/Program.cs
@@ -2,45 +2,86 @@
 
 namespace Practice.Chapter3.ClassesAndObjects
 {
-    class Person
+    class Date
     {
         public string name;
+        public string surname;
         public int age;
+        public double weight;
+        public double height;
 
-        public Person() : this("Неизвестно")
+        public Date(string a, string b, int c, double z, double x) { name=a; surname=b; age=c; weight = z; height = x; }
+        /*public Date(string b, int c) { surname=b; age = c; }
+        public Date(int c, double z) { age=c; weight=z; }
+        public Date(double z, double x) { weight=z; height=x; }*/
+        
+        public void GetConditions()
         {
-        }
-        public Person(string name) : this(name, 41)
-        {
-        }
-        public Person(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-            }
-
-        public void GetInfo()
-        {
-            Console.WriteLine($"Имя: {name} Возраст: {age}");
+            Console.WriteLine("Введите анкетные данные:");
+            Console.WriteLine($"Имя:{name} \nФамилия:{surname} \nВозраст:{age} \nВес:{weight} \nРост:{height} ");
         }
 
         class Program
         {
-            static void Main(string[] args)
-            {
-                Person leo = new Person {name = "leo", age = 43};
-                Person noy = new Person {name = "noy", age = 44};
-                Person loy = new Person {name = "loy", age = 45};
+        }
 
-                leo.GetInfo();
-                noy.GetInfo();
-                loy.GetInfo();
+        static void Main(string[] args)
+        {
+            Date a = new Date("Леонид", "Тархан", 41, 74.5, 176.8); 
+            /*Date b = new Date("Тархан", 41);
+            Date c = new Date(41, 74.5);
+            Date x = new Date(74.5, 176.8);*/
 
-                Console.ReadKey();
-            }
+            a.GetConditions();
+            /*b.GetConditions();
+            c.GetConditions();
+            x.GetConditions();*/
+            Console.ReadKey();
         }
     }
 }
+
+/*class Person
+{
+    public string name;
+    public int age;
+
+    public Person() : this("Неизвестно")
+    {
+    }
+    public Person(string name) : this(name, 41)
+    {
+    }
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        }
+
+    public void GetInfo()
+    {
+        Console.WriteLine($"Имя: {name} Возраст: {age}");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваши данные: ");
+
+            Person leo = new Person {name = "leo", age = 43};
+            Person noy = new Person {name = "noy", age = 44};
+            Person loy = new Person {name = "loy", age = 45};
+
+            leo.GetInfo();
+            noy.GetInfo();
+            loy.GetInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
+}*/
 
 
 
