aaa4823fc70882e98795c254383f6c2c65aba1a6
Leonid <shnayder.801@mail.ru>
2019-10-14 23:35:49 +0300

    Создал проект с использовантем структуры

---
+++ Chapter3/Practice.Chapter3.Structure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice.Chapter3.Structure
+{
+    struct User
+    {
+        public string name;
+        public int age;
+
+        public User(string n, int a)
+        {
+            name = n;
+            age = a;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"{name} - {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[2];
+            users[0] = new User("Bob", 23);
+            //users[0].age = 23;
+
+            users[1].name = "Sam";
+            users[1].age = 43;
+            foreach (User User in users)
+            {
+                User.GetInfo();
+            }
+        
+            User tom =new User("Tom", 33);
+            //tom.name = "Tom";
+            //tom.age = 33;
+
+            tom.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
