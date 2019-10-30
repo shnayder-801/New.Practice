30d64aed105c836ce89591f1944cc56d2accf46e
Leonid <shnayder.801@mail.ru>
2019-10-30 21:22:19 +0300

    Решение задачи с подключением пространства имен

---
+++ Chapter3/Practice.Chapter3.Namespacess/Program.cs
@@ -0,0 +1,50 @@
+﻿/*// условие задачи (создать объект класса Baz)
+
+using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+namespace Foo
+{
+    namespace Boo
+    {
+        class Baz
+        {
+
+        }
+    }
+}*/
+
+using System;
+using Foo.Boo;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baz baz= new Baz();
+            baz.name = "Leonid";
+            Console.WriteLine(baz.name);
+            Console.ReadKey();
+        }
+    }
+}
+namespace Foo
+{
+    namespace Boo
+    {
+        class Baz
+        {
+            public string name;
+        }
+    }
+}
