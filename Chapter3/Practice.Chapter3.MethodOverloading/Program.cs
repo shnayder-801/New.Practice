b2935f8c18083748e73e1c17d60722a82990470e
Leonid <shnayder.801@mail.ru>
2019-11-12 21:09:58 +0300

    Изучение способа перегрузки метода

---
+++ Chapter3/Practice.Chapter3.MethodOverloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice.Chapter3.MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate raf = new Calculate();
+            raf.Cal(4, 5);
+            raf.Cal(234, 25, 45);
+            raf.Cal(234, 25, 4.4);
+            Console.ReadKey();
+        }
+    }
+    class Calculate
+        {
+            public void Cal(int x, int y)
+            {
+                int result = x + y;
+                Console.WriteLine($"Result is: {result}");
+            }
+            public void Cal(int x, int y, int b)
+                {
+                    int result = x + y - b;
+                    Console.WriteLine($"Result is: {result}");
+            }
+            public void Cal(int x, int y, double b)
+            {
+                double result = x + y - b;
+                Console.WriteLine($"Result is: {result}");
+        }
+    }
+}
